1585a3b77d1522c85ffe7cdf0c8d3887fcfe9151
Angry Fennec <shellfix@gmail.com>
2021-12-22 15:25:06 +0300

    Добавляет страницу 500

--- src/pages/Error500Page/Error500Page.tsx
+++ src/pages/Error500Page/Error500Page.tsx
@@ -5,7 +5,9 @@ import { Title } from '../../components/Title';
 const b = block('error500-page');
 
 export const Error500Page = () => (
-  <div className={b()}>
-    <Title tag="h1" className={b('title')} text="500" />
-  </div>
-);
+    <div className={b()}>
+      <Title tag="h1" className={b('title')} text="500"/>
+      <Title tag="h2" className={b('subtitle')} text="Сервер не отвечает"/>
+      <p className={b('text')}>Но все будет хорошо!</p>
+    </div>
+  );
