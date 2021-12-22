7f46afd6fc634974413e5883846077fbf8ab05ce
Angry Fennec <shellfix@gmail.com>
2021-12-22 15:41:23 +0300

    УБирает хук в компонент

--- src/pages/Error404Page/Error404Page.tsx
+++ src/pages/Error404Page/Error404Page.tsx
@@ -6,12 +6,15 @@ import { Button } from '../../components/Button';
 import './error404Page.css';
 
 const b = block('error404-page');
-const navigate = useNavigate();
 
-const onBtnClick = () => {
-  navigate('/');
-}
-export const Error404Page = () => (
+export const Error404Page = () => {
+  const navigate = useNavigate();
+
+  const onBtnClick = () => {
+    navigate('/');
+  }
+
+  return (
     <div className={b()}>
       <Title tag="h1" className={b('title')} text="404"/>
       <Title tag="h2" className={b('subtitle')} text="Не туда попали"/>
@@ -26,3 +29,4 @@ export const Error404Page = () => (
       />
     </div>
   );
+}
--- src/pages/Error500Page/Error500Page.tsx
+++ src/pages/Error500Page/Error500Page.tsx
@@ -1,6 +1,7 @@
 import block from 'bem-cn';
 import React from 'react';
 import { Title } from '../../components/Title';
+import './error500Page.css';
 
 const b = block('error500-page');
 
@@ -8,6 +9,6 @@ export const Error500Page = () => (
     <div className={b()}>
       <Title tag="h1" className={b('title')} text="500"/>
       <Title tag="h2" className={b('subtitle')} text="Сервер не отвечает"/>
-      <p className={b('text')}>Но все будет хорошо!</p>
+      <p className={b('text')}>Но всё будет хорошо!</p>
     </div>
   );
