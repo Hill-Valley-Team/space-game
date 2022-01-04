362642909436be399e6af3f912b1a32b7f8dbda1
Natalya Garipova <webvakansii@yandex.ru>
2022-01-04 18:36:44 +0300

    after mentor review

--- src/game/engine/GameObjects/Sprite/Sprite.ts
+++ src/game/engine/GameObjects/Sprite/Sprite.ts
@@ -2,38 +2,22 @@ import { GameObject } from '../GameObject/GameObject';
 import { SpriteProps } from '../GameObject/types';
 
 export class Sprite extends GameObject {
-  private _source: HTMLImageElement;
+  public source: HTMLImageElement;
 
-  private _width: number | null;
+  public width: number | null;
 
-  private _height: number | null;
+  public height: number | null;
 
-  private _frame: number;
-
-  public get width() {
-    return this._width;
-  }
-
-  public get height() {
-    return this._height;
-  }
-
-  public get source() {
-    return this._source;
-  }
-
-  public get frame() {
-    return this._frame;
-  }
+  public frame: number;
 
   constructor(props: SpriteProps) {
     const { scene, x, y, key, source, width, height, frame } = props;
 
     super({ scene, x, y, key });
-    this._source = source;
-    this._width = width ?? 0;
-    this._height = height ?? 0;
-    this._frame = frame ?? 0;
+    this.source = source;
+    this.width = width ?? 0;
+    this.height = height ?? 0;
+    this.frame = frame ?? 0;
   }
 
   public getProps() {
