61bfa606ecf7f57b2285968ca9a8755f6f49fc5f
Natalya Garipova <webvakansii@yandex.ru>
2022-01-11 21:19:36 +0300

    fix-path-error

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('dist'));
 
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
