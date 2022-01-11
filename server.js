17214ebcaa364ab5b9f8c41e0e6c5f1ad007d1e6
Natalya Garipova <webvakansii@yandex.ru>
2022-01-11 19:41:49 +0300

    add-build

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static('dist'));
 
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(PORT, () => {
