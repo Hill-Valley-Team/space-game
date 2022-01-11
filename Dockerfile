a255b7b6087320d51c57605232e83703ba97585d
Natalya Garipova <webvakansii@yandex.ru>
2022-01-11 17:41:28 +0300

    init

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM alpine:3.14
+WORKDIR /var/www
+
+COPY . .
+RUN npm install
+EXPOSE 3000
+CMD node server.js
---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.static('dist'));
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+app.listen(PORT);
