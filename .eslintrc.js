bbfbf0220a91dc155bd8af5d16fdc47d48969179
Natalya Garipova <42203392+diglibum@users.noreply.github.com>
2022-01-13 14:11:38 +0300

    Add app navigation (#19)
    * user-data-in-profile-page
    
    * add-async-to-getUserData
    
    * add-edit-password-api
    
    * profile-api-added
    
    * added-error-boundary
    
    * added-profile-page-header
    
    * changed-secondary-btn-bg
    
    * added-gitignore-files
    
    * remove DS_Store
    
    * remove DS_Store
    
    * remove DS_Store
    
    * remove .vscode
    
    * after ream review
    
    * init
    ''
    
    * disable-react/require-default-props

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,7 @@ module.exports = {
     'react/function-component-definition': 'off',
     'react/jsx-props-no-spreading': 'off',
     'react/button-has-type': 'off',
+    'react/require-default-props': 'off',
     'react/destructuring-assignment': 'off',
     'jsx-a11y/label-has-associated-control': [
       'error',
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,36 +1,17 @@
 import block from 'bem-cn';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Button } from '../../components/Button';
+import React from 'react';
+import { AppNavigation } from '../../components/AppNavigation/AppNavigation';
 import { PageContainer } from '../../components/PageContainer';
 import { Title } from '../../components/Title';
-import { authController } from '../../controllers/AuthController';
+import './homePage.css';
 
 const b = block('home-page');
 
-export const HomePage = () => {
-  const [requestError, setRequestError] = useState('');
-  const navigate = useNavigate();
-
-  const onClickHandle = () => {
-    authController
-      .logOut()
-      .then(() => navigate('/login'))
-      .catch((error) => {
-        if (error.response && error.response.data) {
-          setRequestError(error.response.data.reason ?? '');
-        }
-      });
-  };
-
-  return (
-    <div className={b()}>
-      <PageContainer size="large">
-        <Title text="Главная страница" />
-        <Button text="Выйти" type="button" onClick={onClickHandle} />
-        HomePage
-        <div>{requestError}</div>
-      </PageContainer>
-    </div>
-  );
-};
+export const HomePage = () => (
+  <div className={b()}>
+    <PageContainer size="large">
+      <Title text="Главная страница" />
+      <AppNavigation className={b('navigation')} align="center" />
+    </PageContainer>
+  </div>
+);
