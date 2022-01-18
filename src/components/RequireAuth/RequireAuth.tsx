bc87709a95a3b54da8f82c5635087557bbb1fc27
Natalya Garipova <webvakansii@yandex.ru>
2022-01-18 11:15:20 +0300

    added-loading-spinner

--- src/components/RequireAuth/RequireAuth.tsx
+++ src/components/RequireAuth/RequireAuth.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useGetUserInfoQuery } from '../../services/UserService';
+import { Loading } from '../Loading';
 
 type RequireAuthProps = {
   children: JSX.Element;
@@ -14,7 +15,7 @@ export const RequireAuth = (props: RequireAuthProps) => {
   const location = useLocation();
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return <Loading />;
   }
 
   if (isSuccess === requireAuth) {
