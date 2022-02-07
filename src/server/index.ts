b7d2dd87e1be0c877d3e98a532c9803c775f4e66
Natalya Garipova <webvakansii@yandex.ru>
2022-02-07 12:05:36 +0300

    added-logger

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,10 +2,13 @@ import path from 'path';
 import express from 'express';
 import render from './middlewares/render';
 import hrm from './middlewares/hrm';
+import logger from './middlewares/logger';
 
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../dist')));
+
+app.use(logger);
 
 app.get('/*', hrm, render);
 
