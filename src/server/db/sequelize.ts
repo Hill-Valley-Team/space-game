654768264ed514f397c23d01a76bc21e29a4c5b2
Natalya Garipova <webvakansii@yandex.ru>
2022-02-22 10:49:08 +0300

    fix-env-config

--- src/server/db/sequelize.ts
+++ src/server/db/sequelize.ts
@@ -1,14 +1,7 @@
 import { Sequelize } from 'sequelize-typescript';
 import { sequelizeOptions } from './config';
 
-export const sequelize = new Sequelize({
-  host: 'localhost',
-  port: 5432,
-  username: 'pgadmin',
-  password: 'Ghbdtn43#',
-  database: 'space-game-db',
-  dialect: 'postgres',
-});
+export const sequelize = new Sequelize(sequelizeOptions);
 
 export async function dbConnect() {
   try {
