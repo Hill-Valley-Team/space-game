bb13861fb5ca48a73b5b6d65b18f4d1fe7f51a2a
Natalya Garipova <webvakansii@yandex.ru>
2022-02-26 20:44:26 +0300

    added-themes

--- postcss.config.js
+++ postcss.config.js
@@ -1,31 +1,8 @@
-const colors = {
-  'primary-color': '#35a7ee',
-  'secondary-color': '#1b305a',
-  'text-color': '#000000',
-  'error-text-color': '#ff4d00',
-  'text-color-light': '#999999',
-  'primary-bg-color': 'rgb(255 255 255 / 90%)',
-  'secondary-btn-bg-color': 'rgb(255 255 255 / 40%)',
-  'disabled-bg-color': '#999999',
-  'accent-color': '#ffc701',
-  'pure-white': '#ffffff',
-  'error-text-color': '#FF4D00',
-  'error-text-color-light': '#fff501',
-  'light-blue': '#daecf8',
-  'purple': '#5d32bb',
-  'orange': '#ff8652',
-};
-
 module.exports = {
   plugins: [
     ['postcss-preset-env', {}],
     ['postcss-nested', { preserveEmpty: true }],
     ['autoprefixer', { grid: true }],
-    [
-      'postcss-simple-vars',
-      {
-        variables: () => colors,
-      },
-    ],
+    ['postcss-advanced-variables', {}],
   ],
 };
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,21 @@
-import block from 'bem-cn';
 import { hot } from 'react-hot-loader/root';
 import { Meta } from 'components/Meta';
 import React from 'react';
 import { AppRoutes } from '../AppRoutes';
 import './app.css';
+import { useAppSelector } from 'hooks/hooks';
 // import { enableServiceWorker } from '../../utils/service-worker';
 
-const b = block('app');
-
-const App = () => (
-  // enableServiceWorker();
-  <div className={b()}>
-    <Meta />
-    <AppRoutes />
-  </div>
-);
+const App = () => {
+  const theme = useAppSelector((store) => store.theme.data?.theme);
+  return (
+    // enableServiceWorker();
+    <div className={`app theme-${theme ?? 'light'}`}>
+      <Meta />
+      <AppRoutes />
+    </div>
+  );
+};
 
 const Component = hot(App);
 
